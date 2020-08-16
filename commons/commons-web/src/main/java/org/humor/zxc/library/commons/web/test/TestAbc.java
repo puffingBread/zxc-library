c7cc6847c323bfe3edc6053d1e882d9ad6ba3e0a
xuzz <xuzz@yoozoo.com>
2020-08-16 23:32:02 +0800

    init

---
+++ commons/commons-web/src/main/java/org/humor/zxc/library/commons/web/test/TestAbc.java
@@ -0,0 +1,16 @@
+package org.humor.zxc.library.commons.web.test;
+
+public class TestAbc {
+
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=9;i++){
+            for(j=1;j<=9;j++){
+                int m;
+                m=i*j;
+
+                System.out.println( i + "*" + j+"="+m);
+            }
+        }
+    }
+}
